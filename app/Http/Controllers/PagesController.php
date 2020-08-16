be8e71edd9ee812074467df2c98a983cdbf31264
Ayomide Adebayo <ayomide.oyemade19@gmail.com>
2020-08-16 11:59:17 +0100

    latest dashboard pages

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -33,6 +33,49 @@ class PagesController extends Controller
 
     public function dashboard(){
 
-        return view('dashboard.index');
+        return view('dashboard.ph-merged');
+    }
+
+
+    public function dashboardActivate(){
+
+        return view('dashboard.account-activate');
+    }
+
+//This shows the merg
+    public function showPH(){
+
+        return view('dashboard.ph-approve');
+    }
+
+
+//This return the gh pending page view
+
+    public function ghPending(){
+
+         return view('dashboard.gh-pending');        
+    }
+
+//This return the ph pending page view
+
+     public function phPending(){
+
+         return view('dashboard.ph-pending');        
+    }
+
+//GH  make withdrawal where users can enter the amount they
+//want to GH
+
+     public function ghWithdrawal(){
+
+         return view('dashboard.gh-withdrawal');        
+    }
+
+//PH Enter Amount Page where users can enter the amount they
+//want to PH 
+
+    public function phEnterAmount(){
+
+        return view('dashboard.ph-enter-amount');
     }
 }
---
+++ resources/views/dashboard/ph-approve.blade.php
@@ -0,0 +1,137 @@
+@extends('test.layouts.dashboardApp')
+
+            <!-- ============================================================== -->
+            <!-- Start right Content here -->
+            <!-- ============================================================== -->
+    @section('content')
+            <div class="main-content">
+
+                <div class="page-content">
+                    <div class="container-fluid">
+
+                        <!-- start page title -->
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="page-title-box d-flex align-items-center justify-content-between">
+                                    <h4 class="mb-0 font-size-18">Dashboard</h4>
+                                </div>
+                            </div>
+                        </div>
+                        <!-- end page title -->
+
+                           <div class="row">
+                            <div class="col-12">
+                                <div class="card">
+                                    <div class="card-body">
+        
+                                        <h4 class="card-title">Textual inputs</h4>
+                                        <p class="card-title-desc">Here are examples of <code>.form-control</code> applied to each
+                                            textual HTML5 <code>&lt;input&gt;</code> <code>type</code>.</p>
+        
+                                        <div class="form-group row">
+                                            <label for="example-text-input" class="col-md-2 col-form-label">Text</label>
+                                            <div class="col-md-10">
+                                                <input class="form-control" type="text" value="Artisanal kale" id="example-text-input">
+                                            </div>
+                                        </div>
+                                        
+                                        <div class="form-group row">
+                                            <label for="example-date-input" class="col-md-2 col-form-label">Date</label>
+                                            <div class="col-md-10">
+                                                <input class="form-control" type="date" value="2019-08-19" id="example-date-input">
+                                            </div>
+                                        </div>
+                                        <div class="form-group row">
+                                            <label for="example-month-input" class="col-md-2 col-form-label">Month</label>
+                                            <div class="col-md-10">
+                                                <input class="form-control" type="month" value="2019-08" id="example-month-input">
+                                            </div>
+                                        </div>
+                                        <div class="form-group row">
+                                            <label for="example-week-input" class="col-md-2 col-form-label">Week</label>
+                                            <div class="col-md-10">
+                                                <input class="form-control" type="week" value="2019-W33" id="example-week-input">
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div> <!-- end col -->
+                        </div>
+                        <!-- end row -->
+
+                    </div>
+                </div>
+                <!-- end modal -->
+
+                <footer class="footer">
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="col-sm-6">
+                                <script>document.write(new Date().getFullYear())</script> © SecuredInvestment.
+                            </div>
+                            <div class="col-sm-6">
+                                <div class="text-sm-right d-none d-sm-block">
+                                    
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </footer>
+            </div>
+            <!-- end main content-->
+
+        </div>
+        <!-- END layout-wrapper -->
+
+        <!-- Right Sidebar -->
+        <div class="right-bar">
+            <div data-simplebar class="h-100">
+                <div class="rightbar-title px-3 py-4">
+                    <a href="javascript:void(0);" class="right-bar-toggle float-right">
+                        <i class="mdi mdi-close noti-icon"></i>
+                    </a>
+                    <h5 class="m-0">Settings</h5>
+                </div>
+
+                <!-- Settings -->
+                <hr class="mt-0" />
+                <h6 class="text-center mb-0">Choose Layouts</h6>
+
+                <div class="p-4">
+                    <div class="mb-2">
+                        <img src="assets/images/layouts/layout-1.jpg" class="img-fluid img-thumbnail" alt="">
+                    </div>
+                    <div class="custom-control custom-switch mb-3">
+                        <input type="checkbox" class="custom-control-input theme-choice" id="light-mode-switch" checked />
+                        <label class="custom-control-label" for="light-mode-switch">Light Mode</label>
+                    </div>
+    
+                    <div class="mb-2">
+                        <img src="assets/images/layouts/layout-2.jpg" class="img-fluid img-thumbnail" alt="">
+                    </div>
+                    <div class="custom-control custom-switch mb-3">
+                        <input type="checkbox" class="custom-control-input theme-choice" id="dark-mode-switch" data-bsStyle="assets/css/bootstrap-dark.min.css" data-appStyle="assets/css/app-dark.min.css" />
+                        <label class="custom-control-label" for="dark-mode-switch">Dark Mode</label>
+                    </div>
+    
+                    <div class="mb-2">
+                        <img src="assets/images/layouts/layout-3.jpg" class="img-fluid img-thumbnail" alt="">
+                    </div>
+                    <div class="custom-control custom-switch mb-5">
+                        <input type="checkbox" class="custom-control-input theme-choice" id="rtl-mode-switch" data-appStyle="assets/css/app-rtl.min.css" />
+                        <label class="custom-control-label" for="rtl-mode-switch">RTL Mode</label>
+                    </div>
+
+            
+                </div>
+
+            </div> <!-- end slimscroll-menu-->
+        </div>
+
+        <!-- Right bar overlay-->
+        
+        <div class="rightbar-overlay"></div>
+    @endsection
+        <!-- /Right-bar -->
+
+        				
