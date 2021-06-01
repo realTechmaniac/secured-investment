2d91f88856c4660d363530cac5b7e13051a8e6d8
Ayomide Adebayo <48103357+realTechmaniac@users.noreply.github.com>
2021-06-01 09:36:19 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+
+//To prevent insecure links -->
+
+if (App::environment('production')) {
+
+    URL::forceScheme('https');
+    
+}
+
 //Index page route handler
 //Route::get('/test','PagesController@index');
 
